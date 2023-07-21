4bf2330087a68205d821e9f51e2ff302bd83ba8a
ashcfeng <140179966+ashcfeng@users.noreply.github.com>
2023-07-21 09:13:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function generateReport() {
+    const childName = document.getElementById("childName").value;
+    const skillsWorkedOn = document.getElementById("skillsWorkedOn").value;
+    const skillsMastered = document.getElementById("skillsMastered").value;
+    const skillsToPractice = document.getElementById("skillsToPractice").value;
+
+    const reportTemplate = `Dear Parent,
+    Today ${childName} worked on ${skillsWorkedOn}. They made lots of progress on ${skillsMastered} and we will continute to help ${childName} get a better understanding of ${skillsToPractice}. Please let us know if you have any questions!`;
+
+    document.getElementById("reportOutput").innerText = reportTemplate;
+    const copyButton = document.getElementById("copyButton");
+    copyButton.style.display = "block";
+    copyButton.textContent = "Copy Report";
+    copyButton.addEventListener("click", copyReport);
+}
+
+function copyReport() {
+    const reportText = document.getElementById("reportOutput").innerText;
+    const textarea = document.createElement("textarea");
+    textarea.value = reportText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+
+    const copyButton = document.getElementById("copyButton");
+    copyButton.textContent = "Copied!";
+    copyButton.removeEventListener("click", copyReport);
+}
+
+
+
+
+
+
