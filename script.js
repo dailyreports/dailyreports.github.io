ba7c023649ca9184e27eb2ddfa6435455d9a16f4
dailyreports <140179966+dailyreports@users.noreply.github.com>
2023-07-22 15:03:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function generateReport() {
+    const templateSelect = document.getElementById("templateSelect");
+    const selectedTemplate = templateSelect.value;
+
+    if (!selectedTemplate) {
+        alert("Please select a template before generating.");
+        return;
+    }
     const childName = document.getElementById("childName").value;
     const skillsWorkedOn = document.getElementById("skillsWorkedOn").value;
     const skillsMastered = document.getElementById("skillsMastered").value;
-    const skillsToPractice = document.getElementById("skillsToPractice").value;
-
-    const reportTemplate = `Hello! Today ${childName} worked on ${skillsWorkedOn}. They made lots of progress on ${skillsMastered} and we will continue to help ${childName} get a better understanding of ${skillsToPractice}. Please let us know if you have any questions!`;
+    const skillsToPractice =      document.getElementById("skillsToPractice").value;
+    let reportTemplate = "";
+    if (selectedTemplate === "today") {
+        reportTemplate = `Hello! Today ${childName} worked on ${skillsWorkedOn}. They made lots of progress on ${skillsMastered} and we will continue to help ${childName} get a better understanding of ${skillsToPractice}. Please let us know if you have any questions!`;
+        
+    } else {
+       if (selectedTemplate === "yesterday") {
+        reportTemplate = `Good morning! Yesterday ${childName} worked on ${skillsWorkedOn}. They made lots of progress on ${skillsMastered} and we will continue to help ${childName} get a better understanding of ${skillsToPractice}. Please let us know if you have any questions!`;
+    }
+    }
 
     document.getElementById("reportOutput").innerText = reportTemplate;
     const copyButton = document.getElementById("copyButton");
@@ -28,7 +42,13 @@ function copyReport() {
 }
 
 
-
-
-
-
+document.getElementById("templateSelect").addEventListener("change", function () {
+    const inputFields = document.getElementById("inputFields");
+    if (this.value) {
+        inputFields.style.display = "block";
+    } else {
+        inputFields.style.display = "none";
+        document.getElementById("reportOutput").innerText = "";
+        document.getElementById("copyButton").style.display = "none";
+    }
+});
